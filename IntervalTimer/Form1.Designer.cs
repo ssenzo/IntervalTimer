ff1bdbe7a159df3c234ab67005ec1c995149127b
ssenzo <ssenzo@naver.com>
2020-12-25 14:49:58 +0900

    초기 커밋.

---
+++ IntervalTimer/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace IntervalTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trackBarOpacity = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblProcessedTime = new System.Windows.Forms.Label();
+            this.lblCurrentTime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarOpacity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBarOpacity
+            // 
+            this.trackBarOpacity.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.trackBarOpacity.AutoSize = false;
+            this.trackBarOpacity.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.trackBarOpacity.Location = new System.Drawing.Point(0, 103);
+            this.trackBarOpacity.Maximum = 100;
+            this.trackBarOpacity.Minimum = 10;
+            this.trackBarOpacity.Name = "trackBarOpacity";
+            this.trackBarOpacity.Size = new System.Drawing.Size(211, 19);
+            this.trackBarOpacity.TabIndex = 0;
+            this.trackBarOpacity.TabStop = false;
+            this.trackBarOpacity.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarOpacity.Value = 100;
+            this.trackBarOpacity.Scroll += new System.EventHandler(this.trackBarOpacity_Scroll);
+            this.trackBarOpacity.KeyUp += new System.Windows.Forms.KeyEventHandler(this.trackBarOpacity_KeyUp);
+            this.trackBarOpacity.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBarOpacity_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblProcessedTime
+            // 
+            this.lblProcessedTime.AutoSize = true;
+            this.lblProcessedTime.Font = new System.Drawing.Font("굴림", 30F);
+            this.lblProcessedTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblProcessedTime.Location = new System.Drawing.Point(13, 9);
+            this.lblProcessedTime.Name = "lblProcessedTime";
+            this.lblProcessedTime.Size = new System.Drawing.Size(181, 40);
+            this.lblProcessedTime.TabIndex = 1;
+            this.lblProcessedTime.Text = "99:99:99";
+            this.lblProcessedTime.Click += new System.EventHandler(this.lblProcessedTime_Click);
+            this.lblProcessedTime.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.lblProcessedTime.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // lblCurrentTime
+            // 
+            this.lblCurrentTime.AutoSize = true;
+            this.lblCurrentTime.Font = new System.Drawing.Font("굴림", 30F);
+            this.lblCurrentTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblCurrentTime.Location = new System.Drawing.Point(13, 56);
+            this.lblCurrentTime.Name = "lblCurrentTime";
+            this.lblCurrentTime.Size = new System.Drawing.Size(181, 40);
+            this.lblCurrentTime.TabIndex = 1;
+            this.lblCurrentTime.Text = "99:99:99";
+            this.lblCurrentTime.Click += new System.EventHandler(this.lblCurrentTime_Click);
+            this.lblCurrentTime.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.lblCurrentTime.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(211, 122);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblCurrentTime);
+            this.Controls.Add(this.lblProcessedTime);
+            this.Controls.Add(this.trackBarOpacity);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarOpacity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarOpacity;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblProcessedTime;
+        private System.Windows.Forms.Label lblCurrentTime;
+    }
+}
+
---
+++ IntervalTimer/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntervalTimer
+{
+    public partial class Form1 : Form
+    {
+        private Point mousePoint;
+        private DateTime startDateTime;
+        private string elapsedTimeShowType = "ELAPSED";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBarOpacity_Scroll(object sender, EventArgs e)
+        {
+            this.Text = trackBarOpacity.Value.ToString();
+            this.Opacity = trackBarOpacity.Value/100.0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetElapsedTime();
+
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, 
+                                        Screen.PrimaryScreen.Bounds.Height - this.Height);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousePoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                Location = new Point(this.Left - (mousePoint.X - e.X),
+                    this.Top - (mousePoint.Y - e.Y));
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblCurrentTime.Text = DateTime.Now.ToString("HH:mm:ss");
+
+            TimeSpan timeDiff;
+            
+            if (elapsedTimeShowType == "ELAPSED")
+            {
+                timeDiff = DateTime.Now - startDateTime;
+            }
+            else
+            {
+                timeDiff = startDateTime.AddHours(1) - DateTime.Now;
+            }
+            
+
+            lblProcessedTime.Text = timeDiff.Hours.ToString().PadLeft(2, '0')
+                + ':' + timeDiff.Minutes.ToString().PadLeft(2, '0')
+                + ':' + timeDiff.Seconds.ToString().PadLeft(2, '0');
+        }
+
+        private void resetElapsedTime()
+        {
+            startDateTime = DateTime.Now;
+        }
+
+        private void lblCurrentTime_Click(object sender, EventArgs e)
+        {
+            resetElapsedTime();
+        }
+
+        private void lblProcessedTime_Click(object sender, EventArgs e)
+        {
+            if (elapsedTimeShowType == "ELAPSED")
+            {
+                elapsedTimeShowType = "REMAINED";
+            }
+            else
+            {
+                elapsedTimeShowType = "ELAPSED";
+            }
+        }
+
+        private void trackBarOpacity_MouseUp(object sender, MouseEventArgs e)
+        {
+            lblCurrentTime.Focus();
+        }
+
+        private void trackBarOpacity_KeyUp(object sender, KeyEventArgs e)
+        {
+            lblCurrentTime.Focus();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
